15da4a101dac2f95df081e421464271455eac0bc
Gabriel Ferreira <gabreeiel@gmail.com>
2018-07-19 11:09:18 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/pro/gabrielferreira/cursomc/config/DevConfig.java
+++ src/main/java/pro/gabrielferreira/cursomc/config/DevConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import pro.gabrielferreira.cursomc.services.DBService;
+import pro.gabrielferreira.cursomc.services.EmailService;
+import pro.gabrielferreira.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev") // configuracoes para o perfil de teste
@@ -25,5 +27,12 @@ public class DevConfig {
 		}
 		dbService.instanciateTestDatabase();
 		return true;
+	}
+
+	// esse bean diz que quando eu injetar um EmailService e tiver usando o profile
+	// de dev, ele vai retornar um SmtpEmailService
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
