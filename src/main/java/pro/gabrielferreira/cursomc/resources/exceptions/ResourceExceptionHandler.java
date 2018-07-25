4b4e9a2291c8b90e25cc130b507abd3a0adb96be
Gabriel Ferreira <gabreeiel@gmail.com>
2018-07-25 15:30:24 -0300

    tratando exceptions adequadamente

--- src/main/java/pro/gabrielferreira/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/pro/gabrielferreira/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -9,27 +9,33 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+
 import pro.gabrielferreira.cursomc.services.exceptions.AuthorizationException;
 import pro.gabrielferreira.cursomc.services.exceptions.DataIntegrityException;
+import pro.gabrielferreira.cursomc.services.exceptions.FileException;
 import pro.gabrielferreira.cursomc.services.exceptions.ObjectNotFoundException;
 
 //nao entendi muito bem, mas funciona, faz com que retorne uma msg de erro mais bonitinha.
 @ControllerAdvice
 public class ResourceExceptionHandler {
-	
-	@ExceptionHandler //tratamento de erro para 404 not found
-	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+
+	@ExceptionHandler(ObjectNotFoundException.class) // tratamento de erro para 404 not found
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
-	
-	@ExceptionHandler //tratamento de erro para bad request 400
-	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
-		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+
+	@ExceptionHandler(DataIntegrityException.class) // tratamento de erro para bad request 400
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 
-	@ExceptionHandler // tratamento para erros de validacao
+	@ExceptionHandler(MethodArgumentNotValidException.class) // tratamento para erros de validacao
 	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
 
 		// instancio o erro, ainda sem a lista de erros
@@ -41,13 +47,41 @@ public class ResourceExceptionHandler {
 		for (FieldError x : e.getBindingResult().getFieldErrors()) {
 			err.addError(x.getField(), x.getDefaultMessage());
 		}
-		
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(AuthorizationException.class) // tratamento de erro para 404 not found
+	public ResponseEntity<StandardError> authorization(AuthorizationException e, HttpServletRequest request) {
+		StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+	}
+
+	@ExceptionHandler(FileException.class) // tratamento de erro para 404 not found
+	public ResponseEntity<StandardError> file(FileException e, HttpServletRequest request) {
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(AmazonServiceException.class)
+	public ResponseEntity<StandardError> amazonService(AmazonServiceException e, HttpServletRequest request) {
+		HttpStatus code = HttpStatus.valueOf(e.getErrorCode());
+		StandardError err = new StandardError(code.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(code).body(err);
+	}
+
+	@ExceptionHandler(AmazonClientException.class)
+	public ResponseEntity<StandardError> amazonClient(AmazonClientException e, HttpServletRequest request) {
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 	
-	@ExceptionHandler //tratamento de erro para 404 not found
-	public ResponseEntity<StandardError> authorization(AuthorizationException e, HttpServletRequest request){
-		StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
-		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+	@ExceptionHandler(AmazonS3Exception.class)
+	public ResponseEntity<StandardError> amazozS3(AmazonS3Exception e, HttpServletRequest request) {
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
---
+++ src/main/java/pro/gabrielferreira/cursomc/services/exceptions/FileException.java
@@ -0,0 +1,14 @@
+package pro.gabrielferreira.cursomc.services.exceptions;
+
+//erro personalizado, apenas extends runtime..
+public class FileException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public FileException(String msg) {
+		super(msg);
+	}
+	
+	public FileException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
