cccb55d76b149edf3980ddba44de910c2d67ce97
Gabriel Ferreira <gabreeiel@gmail.com>
2018-07-25 14:38:29 -0300

    Salvando arquivos no S3

--- src/main/java/pro/gabrielferreira/cursomc/CursomcApplication.java
+++ src/main/java/pro/gabrielferreira/cursomc/CursomcApplication.java
@@ -1,19 +1,25 @@
 package pro.gabrielferreira.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import pro.gabrielferreira.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	/* Classe principal do spring boot */
-
+	
+	@Autowired
+	private S3Service s3service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) {
-
+	public void run(String... args) throws Exception{
+		s3service.uploadFile("/home/gabriel/Pictures/mulher.jpg");
 	}
 }
